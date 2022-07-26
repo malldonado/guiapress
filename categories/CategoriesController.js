204b1d46aed03675e8aa9fc9b3b6b7975dd4161f
Matheus Maldonado <matheussilvacontat@gmail.com>
2022-07-26 01:34:19 -0300

    pronto

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,5 +1,8 @@
 const express = require('express'); 
 const router = express.Router();
+const Category = require('./Category');
+const slugify = require('slugify'); // slugify is a format of get value or string and transforms into string tiny with arrows,
+                                    //example: desenvolvimento-web
 
 router.get('/admin/categories/new', (req, res) =>{
     res.render('admin/categories/new');
@@ -12,6 +15,51 @@ router.get('/categories/save', (req, res) =>{
     } else {
         res.redirect('/admin/categories/new');
     }
+});
+
+//the form is ever post method
+router.post('/categories/save', (req, res) =>{
+    var title = req.body.title;
+    if(title != undefined){
+        Category.create({
+            title: title,
+            slug: slugify(title)
+        }).then(() => {
+            res.redirect('/');
+        })
+    } else {
+        res.redirect('/admin/categories/new');
+    }
+});
+
+router.get('/admin/categories', (req, res) =>{
+
+    Category.findAll().then(categories => {
+        res.render('admin/categories/index', {categories: categories}); //move categories to front-end, format json
+    })
+
+});
+
+router.post('/categories/delete', (req, res) =>{
+    var id = req.body.id;
+
+    if(id != undefined){
+        if(!isNaN(id)) { //is number
+            Category.destroy({
+                where: {
+                    id: id //remove elemento by id
+                }
+            }).then(() => {
+                res.redirect('/admin/categories');
+            })
+
+        } else {// is not a number
+            res.redirect('/admin/categories');
+        }
+    } else { //null
+        res.redirect('/admin/categories');
+    }
+
 });
 
 module.exports = router;
